908be86ef07552c8a68f8fab03b3c3cd5fd69524
Stanislav Ershov <ershov.stanislav@gmail.com>
2014-11-09 14:50:10 +0300

    new commit

---
+++ gameOfLife/src/Animation.java
@@ -0,0 +1,65 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class Animation extends JLabel implements ActionListener {
+    public Field game;
+    public boolean stopped = false;
+    Timer timer;
+    final Color gr = new Color(0, 128, 0);
+    int speedUp=1, width=50, height=50;
+
+    public Animation() {
+        this.setBackground(Color.WHITE);
+
+        game = new Field();
+        timer = new Timer(20, this);
+        timer.setInitialDelay(40);
+        timer.start();
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        if (stopped) return;
+        for (int i=0; i<Math.max(1, speedUp); i++) game.step();
+        repaint();
+    }
+
+    public void setFPS(int fps) {
+        if (fps>50) timer.setDelay(20);
+        else timer.setDelay(1000/fps);
+        speedUp=fps/50;
+        timer.restart();
+    }
+
+    public void setWidth(int w) {
+        width=height=w;
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Dimension d = getSize();
+
+        double w = width, h = height, grid_x = d.width / w, grid_y = d.height / h;
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (game.f[i][j] == true) {
+                    g2.setPaint(gr);
+                    g2.fill(new Ellipse2D.Double(grid_x * (j + 0.1), grid_y * (i + 0.1), grid_x * 0.8, grid_y * 0.8));
+                }
+            }
+        }
+        g2.setPaint(new Color(128, 128, 128));
+        if (grid_x > 5 && grid_y > 5) {
+            for (int i = 1; i < w; i++) {
+                g2.draw(new Line2D.Double(i * grid_x, 0, i * grid_x, d.height));
+            }
+            for (int i = 1; i < h; i++) {
+                g2.draw(new Line2D.Double(0, i * grid_y, d.width, i * grid_y));
+            }
+        }
+    }
+}
---
+++ gameOfLife/src/Main.java
@@ -0,0 +1,91 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Main extends JPanel implements ActionListener, ChangeListener {
+    Animation GameOfLifeField;
+    JButton button;
+    JSlider fieldWidth, framesPerSecond;
+
+    public Main() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        setBackground(Color.WHITE);
+
+        GameOfLifeField = new Animation();
+        GameOfLifeField.setPreferredSize(new Dimension(900, 750));
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx=1;
+        c.weighty=1;
+        add(GameOfLifeField, c);
+        GameOfLifeField.setOpaque(true);
+
+        JPanel right = new JPanel();
+        right.setLayout(new BoxLayout(right, BoxLayout.PAGE_AXIS));
+        button = new JButton("Stop");
+        button.setActionCommand("stop_start");
+        button.addActionListener(this);
+        right.setBackground(Color.WHITE);
+        right.add(button);
+        c.gridx = 1;
+        c.weightx=0;
+        c.weighty=0;
+        c.fill=GridBagConstraints.NONE;
+        c.anchor=GridBagConstraints.NORTH;
+        add(right, c);
+
+        framesPerSecond = new JSlider(JSlider.VERTICAL, 0, 20, 10);
+        GameOfLifeField.setFPS((int)Math.sqrt(1<<10));
+        framesPerSecond.addChangeListener(this);
+        framesPerSecond.setMajorTickSpacing(5);
+        framesPerSecond.setMinorTickSpacing(1);
+        framesPerSecond.setPaintTicks(true);
+        framesPerSecond.setPaintLabels(true);
+        right.add(framesPerSecond);
+
+        fieldWidth = new JSlider(JSlider.VERTICAL, 0, 80, 50);
+        fieldWidth.addChangeListener(this);
+        fieldWidth.setMajorTickSpacing(20);
+        fieldWidth.setMinorTickSpacing(5);
+        fieldWidth.setPaintTicks(true);
+        fieldWidth.setPaintLabels(true);
+        right.add(fieldWidth);
+        GameOfLifeField.setWidth(50);
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        if ("stop_start".equals(e.getActionCommand())) {
+            GameOfLifeField.stopped^=true;
+            button.setText(GameOfLifeField.stopped ? "Start" : "Stop");
+        }
+    }
+
+    public void stateChanged(ChangeEvent e) {
+        JSlider source = (JSlider) e.getSource();
+        if (!source.getValueIsAdjusting()) {
+            if (source.equals(framesPerSecond)) {
+                int fps = source.getValue();
+                GameOfLifeField.setFPS((int) Math.sqrt(1 << fps));
+            } else if (source.equals(fieldWidth)) {
+                int width = source.getValue();
+                GameOfLifeField.setWidth(width);
+            }
+        }
+    }
+
+
+    public static void main(String s[]) {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setContentPane(new Main());
+        f.setTitle("Conway's Game of Life");
+        f.setBackground(Color.WHITE);
+        f.setForeground(Color.WHITE);
+        f.pack();
+
+        f.setVisible(true);
+    }
+}
