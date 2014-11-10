b551fba972bc8e24583665a0b6f633da53a0a88e
BKudrin <bogdan.kudrin@firstlinesoftware.com>
2014-11-10 16:58:17 +0300

    Добавил небольшие комментарии 10.11.2014 в 16.58

--- gameOfLife/src/Animation.java
+++ gameOfLife/src/Animation.java
@@ -3,12 +3,16 @@ import java.awt.event.*;
 import java.awt.geom.*;
 import javax.swing.*;
 
+//Здорово, осталось только ввод живых клеток по клику сделать
 public class Animation extends JLabel implements ActionListener {
     public Field game;
     public boolean stopped = false;
     Timer timer;
     final Color gr = new Color(0, 128, 0);
-    int speedUp=1, width=50, height=50;
+    //Лучше каждую переменную в своей строке писать
+    int speedUp = 1;
+    int width = 50;
+    int height = 50;
 
     public Animation() {
         this.setBackground(Color.WHITE);
@@ -22,33 +26,41 @@ public class Animation extends JLabel implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         if (stopped) return;
-        for (int i=0; i<Math.max(1, speedUp); i++) game.step();
+        // После for ставят фигурные скобки, всегда
+        for (int i = 0; i < Math.max(1, speedUp); i++) {
+            game.step();
+        }
         repaint();
     }
 
     public void setFPS(int fps) {
-        if (fps>25) timer.setDelay(40);
-        else timer.setDelay(1000/fps);
-        speedUp=fps/25;
+        //Всегда фигурные скобки в if
+        if (fps > 25) {
+            timer.setDelay(40);
+        } else {
+            timer.setDelay(1000 / fps);
+        }
+        speedUp = fps / 25;
         timer.restart();
     }
 
     public void setWidth(int w) {
-        width=height=w;
+        width = height = w;
     }
 
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         Dimension d = getSize();
-        
-        g2.clearRect(0,  0,  d.width, d.height);
+
+        g2.clearRect(0, 0, d.width, d.height);
 
         double w = width, h = height, grid_x = d.width / w, grid_y = d.height / h;
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                if (game.f[i][j] == true) {
+                //Можно просто так, сравнение с true избыточно
+                if (game.f[i][j]) {
                     g2.setPaint(gr);
                     g2.fill(new Ellipse2D.Double(grid_x * (j + 0.1), grid_y * (i + 0.1), grid_x * 0.8, grid_y * 0.8));
                 }
