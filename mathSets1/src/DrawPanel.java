42b176d3f2b8fb2edef0a2e4fbabfd62e916b106
Vlasova Nadezhda <evropa2100@mail.ru>
2014-11-16 23:04:59 +0300

    Changes to be committed: 	modified:   mathSets1/src/DrawPanel.java 	modified:   mathSets4/src/DrawPanel.java

--- mathSets1/src/DrawPanel.java
+++ mathSets1/src/DrawPanel.java
@@ -3,9 +3,26 @@
  */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.*;
 
 class DrawPanel extends JPanel {
+
+    int n;
+
+    Timer timer = new javax.swing.Timer( 500, new ActionListener(){
+        public void actionPerformed(ActionEvent e) {
+            n++;
+            repaint();
+        }
+    } );
+
+    public DrawPanel(){
+        n = 1;
+        timer.start();
+    }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g.create();
@@ -14,9 +31,9 @@ class DrawPanel extends JPanel {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setStroke(new BasicStroke(4));
         g2.setPaint(Color.black);
-        g2.fill(new Rectangle2D.Double(0, 0, getWidth(), getHeight()));
+        //g2.fill(new Rectangle2D.Double(0, 0, getWidth(), getHeight()));
 
-        int n = 1000;
+       // int n = 1000;
 
         for ( int x = 0; x < getWidth(); x++)
             for (int y = 0; y < getHeight(); y ++){
@@ -25,13 +42,13 @@ class DrawPanel extends JPanel {
                 double y0 = (y - getHeight() / 2) / 75.0;
                 for ( i = 0; (i < n) && (Math.abs(x0) <= 2) &&(Math.abs(y0) <= 2) ; i++)
                 {
-                    double temp = y0;
-                    y0 = 0.3 * x0;
-                    x0 = 1 + temp / 0.7 - 1.4*x0*x0;
+                    double y01 = 0.3 * x0;
+                    x0 = 1 + y0/0.9 - 1.4*x0*x0;
+                    y0=y01;
 
                 }
                 if ((Math.abs(x0) > 2) ||(Math.abs(y0) > 2) ) {
-                    int col = 255 - i % 255;
+                    int col = i % 255;
                     g2.setPaint(new Color(col,col,col));
                     g2.fill(new Rectangle2D.Double(x, y, 1, 1));
                 }
--- mathSets4/src/DrawPanel.java
+++ mathSets4/src/DrawPanel.java
@@ -4,8 +4,26 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class DrawPanel extends JPanel {
+
+    int n;
+
+    Timer timer = new javax.swing.Timer( 500, new ActionListener(){
+        public void actionPerformed(ActionEvent e) {
+            n++;
+            repaint();
+        }
+    } );
+
+    public DrawPanel(){
+        n = 1;
+        timer.start();
+    }
+
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g.create();
@@ -14,22 +32,23 @@ class DrawPanel extends JPanel {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setStroke(new BasicStroke(4));
         g2.setPaint(Color.black);
-        g2.fill(new Rectangle2D.Double(0, 0, getWidth(), getHeight()));
+        //g2.fill(new Rectangle2D.Double(0, 0, getWidth(), getHeight()));
 
-        int n = 1000;
-        Complex c = new Complex(0.25,0.52);
+       // int n = 1000;
+        Complex c = new Complex(0.377,-0.248);
+
+
 
         for ( int x = 0; x < getWidth(); x++)
             for (int y = 0; y < getHeight(); y ++){
                 Complex z = new Complex((x - getWidth() / 2) / 75.0, (y - getHeight() / 2) / 75.0);
                 int i;
-                for ( i = 0; (i < n) && z.Proverka(); i++)
-                {
+                for ( i = 0; (i < n) && z.Proverka(); i++) {
                     z = z.Sqr();
                     z = z.Sum(c);
                 }
-                if (!z.Proverka()) {//hjhjhj
-                    int col = 255 - i % 255;
+                if (!z.Proverka()) {
+                    int col = i % 255;
                     g2.setPaint(new Color(col,col,col));
                     g2.fill(new Rectangle2D.Double(x, y, 1, 1));
                 }
