53a6dfabccbffb8e56874958c5a0b2ba8e74ff45
petepat <petevuka@gmail.com>
2014-12-08 17:54:53 +0300

    конечная версия
    готово. параметры нужно вводить

---
+++ MathSet1/src/Main.java
@@ -0,0 +1,11 @@
+package mathset;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Window app = new Window();
+		app.frame.setSize(1000, 1000);
+   	    app.frame.setVisible(true);
+	}
+
+}
