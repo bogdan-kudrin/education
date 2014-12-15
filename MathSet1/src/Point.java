719cb2ea515a4c9c0b2bfcfebbee727a8c3a13e2
bogdan-kudrin <bogdan.kudrin@gmail.com>
2014-12-15 23:55:20 +0300

    Update Point.java

--- MathSet1/src/Point.java
+++ MathSet1/src/Point.java
@@ -13,9 +13,7 @@ public class Point {
     }
 
     public void transform() {
-        double y = 0.3 * this.x;
-        this.x = 1 + this.y / 0.9 - 1.4 * this.x * this.x;
-        this.y = y;
+        //Сюда введите отображение Хенона
     }
 
     public boolean outOfField(){
